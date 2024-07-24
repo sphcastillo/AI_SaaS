7810ed170280308c5f813523ddecc49c2fc54fb2
sphcastillo <sphcastillo@gmail.com>
2024-07-24 15:37:54 -0700

    Finished Landing page ~ onto Dashboard page, adding its own layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   BrainCogIcon,
   EyeIcon,
@@ -5,18 +6,117 @@ import {
   MonitorSmartphoneIcon,
   ServerCogIcon,
   ZapIcon,
-} from 'lucide-react';
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const features = [
   {
     name: "Store your PDF Documents",
-    description: 'Keep all your important PDF files securely stored and easily accessible anytime, anywhere.',
-    icon: ZapIcon
-  }
-]
+    description:
+      "Keep all your important PDF files securely stored and easily accessible anytime, anywhere.",
+    icon: GlobeIcon,
+  },
+  {
+    name: "Store your PDF Documents",
+    description:
+      "Keep all your important PDF files securely stored and easily accessible anytime, anywhere.",
+    icon: ZapIcon,
+  },
+  {
+    name: "Store your PDF Documents",
+    description:
+      "Keep all your important PDF files securely stored and easily accessible anytime, anywhere.",
+    icon: BrainCogIcon,
+  },
+  {
+    name: "Store your PDF Documents",
+    description:
+      "Keep all your important PDF files securely stored and easily accessible anytime, anywhere.",
+    icon: GlobeIcon,
+  },
+  {
+    name: "Store your PDF Documents",
+    description:
+      "Keep all your important PDF files securely stored and easily accessible anytime, anywhere.",
+    icon: GlobeIcon,
+  },
+];
 
 export default function Home() {
   return (
-    <main></main>
+    <main className="flex-1 overflow-scroll p-2 lg:p-5 bg-gradient-to-bl from-white to-indigo-600">
+      <div className="bg-white py-24 sm:py-32 rounded-md drop-shadow-xl">
+
+        <div className="flex flex-col justify-center items-center mx-auto max-w-7xl px-6 lg:px-8">
+
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="text-base font-semibold text-indigo-600">
+              Your Interactive Document Companion
+            </h2>
+
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Transform Your PDFs into Interactive Conversations
+            </p>
+
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Introducing{" "}
+              <span className="font-bold text-indigo-600">Chat with PDF.</span>
+              <br/>
+              <br/> Upload your document, and our chatbox will answer questions, summarize content, and answer all your Qs. Ideal for everyone, 
+              <span className="text-indigo-600">
+              {" "} Chat with PDF
+                </span>{" "}
+                turns static documents into {" "}
+                <span className="font-bold">dynamic conversations</span>,
+                enhancing productivity 10x fold effortlessly.
+            </p>
+          </div>
+          {/* Button is a child / asChild prop is needed */}
+          <Button asChild className="mt-10">
+            <Link href='/dashboard'>Get Started</Link>
+          </Button>
+
+        </div>
+
+        <div className="relative overflow-hidden pt-16">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <Image 
+              alt="App screenshot"
+              src="https://i.imgur.com/VciRSTI.jpeg"
+              width={2432}
+              height={1442}
+              className="mb-[-0%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+            />
+            {/* blur effect */}
+            <div 
+              className="relative"
+              aria-hidden="true"
+            >
+              {/* notice requirements */}
+              <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]"/>
+            </div>
+          </div>
+        </div>
+
+        <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+          <dl className="mx-auto grid grid-cols-1 max-w-2xl gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+            {features.map((feature, f) => (
+              <div className="relative pl-9" key={f}>
+
+                <dt className="font-semibold text-gray-900 inline">
+                <feature.icon 
+                    aria-hidden="true"
+                    className="absolute top-1 left-1 h-5 w-5 text-indigo-600"
+                  />
+                </dt>
+                <dd>{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+
+      </div>
+    </main>
   );
 }
