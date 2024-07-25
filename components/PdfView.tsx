cfa0cc9729fa38ea368d91cd7d373701653bd81e
sphcastillo <sphcastillo@gmail.com>
2024-07-25 16:24:23 -0700

    Added buttons on PdfView file

--- components/PdfView.tsx
+++ components/PdfView.tsx
@@ -38,8 +38,68 @@ function PdfView({ url }: { url: string }) {
 
   return (
     <div className="flex flex-col justify-center items-center">
+      <div className="sticky top-0 z-50 bg-gray-100 rounded-b-lg p-2">
+        <div className="max-w-6xl px-2 grid grid-cols-6 gap-2">
+          <Button
+            onClick={() => {
+              if (pageNumber > 1) {
+                setPageNumber(pageNumber - 1);
+              }
+            }}
+            disabled={pageNumber === 1}
+            variant="outline"
+          >
+            Previous
+          </Button>
+          <p className="flex items-center justify-center">
+            {pageNumber} of {numPages}
+          </p>
+          <Button
+            onClick={() => {
+              if (numPages) {
+                if(pageNumber < numPages){
+                    setPageNumber(pageNumber + 1);
+                }
+              }
+            }}
+            disabled={pageNumber === numPages}
+            variant="outline"
+          >
+            Next
+          </Button>
+
+          <Button 
+            variant="outline"
+            onClick={() => {
+              setRoation((roation + 90) % 360);
+            }}
+          >
+            <RotateCw />
+          </Button>
+
+          <Button
+            variant="outline"
+            disabled={scale >= 1.5}
+            onClick={() => {
+              setScale(scale * 1.2);
+            }}
+          >
+            <ZoomInIcon />
+          </Button>
+
+          <Button
+            variant="outline"
+            disabled={scale <= 0.75}
+            onClick={() => {
+              setScale(scale / 1.2);
+            }}
+          >
+            <ZoomOutIcon />
+          </Button>
+        </div>
+      </div>
       {!file ? (
-        <Loader2Icon className="animate-spin h-20 w-20 text-indigo-600 mt-20"/>
+        <Loader2Icon className="animate-spin h-20 w-20 text-indigo-600 mt-20" />
       ) : (
         <Document
           loading={null}
