9bfa8fcaf8185004d58ea048260352d18bc79b20
sphcastillo <sphcastillo@gmail.com>
2024-07-25 21:58:22 -0700

    Working on Chat input form

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -8,9 +8,42 @@ import { useUser } from '@clerk/nextjs';
 import { collection, orderBy, query } from "firebase/firestore";
 import { db } from "@/firebase";
 
-function Chat({id}: { id: string }) {
-  return (
-    <div>Chat</div>
-  )
+export type Message = {
+    id: string;
+    role: "human" | "ai" | "placeholder";
+    message: string;
+    createdAt: Date;
+}
+
+function Chat({ id }: { id: string }) {
+    const { user } = useUser();
+
+    const [input, setInput] = useState('');
+    const [isPending, startTransition] = useTransition();
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+    }
+    return (
+        <div className="flex flex-col h-full overflow-scroll">
+            {/* Chat contents */}
+            <div className="flex-1 w-full">
+            {/* chat messages... */}
+            </div>
+
+            <form
+                className="flex sticky bottom-0 space-x-2 p-5 bg-indigo-600/75"
+                onSubmit={handleSubmit}
+            >
+                <Input 
+                    placeholder="Ask a Question..."
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <Button>Ask</Button>
+            </form>
+        </div>
+    )
 }
 export default Chat;
