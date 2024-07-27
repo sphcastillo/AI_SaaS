2b07139d9593fe82b2df188cf62e61a25ebb865b
sphcastillo <sphcastillo@gmail.com>
2024-07-27 08:00:07 -0700

    4th attempt ~ serviceKey

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps, getApp, App, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey = require("./service_key.json");
+const serviceKey = require('./service_key.json');
 
 // const serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
 let app: App;
