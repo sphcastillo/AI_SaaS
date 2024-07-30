ec4124ffefc95e9c07a4e92c3ee120917e9201a5
sphcastillo <sphcastillo@gmail.com>
2024-07-29 17:21:58 -0700

    adding Toaster to whole web app ~ created UpgradeButton component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className="min-h-screen h-screen overflow-hidden flex flex-col">
+          <Toaster />
           {children}
         </body>
       </html>
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { FilePlus2 } from "lucide-react";
+import UpgradeButton from "./UpgradeButton";
 
 function Header() {
   return (
@@ -29,7 +30,7 @@ function Header() {
                         <FilePlus2 className="text-indigo-600" />
                     </Link>
                 </Button>
-                {/* Upgrade Button */}
+                <UpgradeButton />
                 <UserButton />
             </div>
         </SignedIn>
